5ff8b38c91222de7a6ffbd9e8ee71d803ab5da68
Nicolas Espinoza <nico.dsgrafico@gmail.com>
2024-05-14 08:38:54 -0400

    :tada: Carpetas iniciales y ruta a index inicial

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import path from "path";
+
+const router = express.Router();
+const __dirname = path.resolve();
+
+router.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+})
+
+
+
+export default router;
