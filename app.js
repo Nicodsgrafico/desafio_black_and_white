4dc76c53e16cc6f69f04c73755ef4ce34ace4405
Nicolas Espinoza <nico.dsgrafico@gmail.com>
2024-05-14 10:56:14 -0400

    :sparkles: Desafio terminado

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ import  routes  from "./routes/routes.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use("/", routes);
+//Carpeta publica estatica
 
+app.use(express.static("assets"));
+//app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", routes);
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
